55cfae89c4b43124b6c94451ae979b0e1fec132f
Huzaifa <godriednuts@gmail.com>
2025-12-29 00:10:13 +0500

    feat: implement static file serving and health check endpoint

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK. Deployed via Travis CI!', uptime: process.uptime() });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 if (require.main === module) {
